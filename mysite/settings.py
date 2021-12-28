317cd39d23d2a2c8b0d369a7de484fb4d61b4a79
ishanmongia20 <imongia14@gmail.com>
2021-12-28 14:18:14 +0530

    require

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,6 +125,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
