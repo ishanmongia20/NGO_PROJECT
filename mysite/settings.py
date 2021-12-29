a8678df42b3d4127b08b2425a1b0679ec98f9dcc
ishanmongia20 <imongia14@gmail.com>
2021-12-29 10:05:09 +0530

    change whitenoise

--- mysite/settings.py
+++ mysite/settings.py
@@ -137,8 +137,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
-# STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
